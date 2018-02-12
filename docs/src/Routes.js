c8d9aa7b36669a5395e3cd814e5603627db5fed2
johncoffee <github2017@ragelse.dk>
2018-02-12 23:35:07 +0100

    build gh pages

--- docs/src/Routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Routes;
-(function (Routes) {
-    Routes[Routes["PLANKE"] = 1] = "PLANKE";
-    Routes[Routes["SOUND_BOARD"] = 2] = "SOUND_BOARD";
-    Routes[Routes["GLOSSARY"] = 3] = "GLOSSARY";
-})(Routes = exports.Routes || (exports.Routes = {}));
--- docs/src/Routes.ts
+++
@@ -1,6 +0,0 @@
-export enum Routes {
-  PLANKE = 1,
-  SOUND_BOARD,
-  GLOSSARY,
-}
-
