3c0c6b21c196369db6b59ba6ebae4e27bca7134e
johncoffee <github2017@ragelse.dk>
2018-02-12 21:00:42 +0100

    unused route

--- src/Routes.js
+++ src/Routes.js
@@ -5,5 +5,4 @@ var Routes;
     Routes[Routes["PLANKE"] = 1] = "PLANKE";
     Routes[Routes["SOUND_BOARD"] = 2] = "SOUND_BOARD";
     Routes[Routes["GLOSSARY"] = 3] = "GLOSSARY";
-    Routes[Routes["NOTARIUSZ"] = 4] = "NOTARIUSZ";
 })(Routes = exports.Routes || (exports.Routes = {}));
