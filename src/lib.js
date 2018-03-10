edacb23efeac16037acad86b5c9b15420b10012e
johncoffee <github2017@ragelse.dk>
2018-03-10 16:43:28 +0000

    dont play sound on full screeen

--- src/lib.js
+++ src/lib.js
@@ -34,11 +34,6 @@ async function loadContent () {
 }
 
 function toggleFullScreen () {
-  if (!sessionStorage.hasPlayed) {
-    sessionStorage.hasPlayed = 1
-    play_vi_venter_lige_paa_oliver()
-  }
-
   if ((document.fullScreenElement && document.fullScreenElement !== null) ||
     (!document.mozFullScreen && !document.webkitIsFullScreen)) {
     if (document.documentElement.requestFullScreen) {
